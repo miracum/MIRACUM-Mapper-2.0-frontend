bc57b4d10f04def800f7ca09bc5f4ff3565b0d09
henrik <henrik.herzig@gmx.de>
2024-06-28 13:17:45 +0200

    stay logged in

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    isAuthenticated: false,
+    isAuthenticated: !!localStorage.getItem('bearerToken'),
     // Retrieve the bearer token from localStorage or set it to an empty string if not found
     bearerToken: localStorage.getItem('bearerToken') || ''
   }),
